8c69d43f15db212f96571e25280afc7cf3824fbf
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-03-07 09:16:42 +0100

    remove unused icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState, useRef, useEffect } from 'react';
-import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import image from '/frida.jpg';
 import Slider from "./components/Slider";
@@ -57,9 +56,6 @@ function App() {
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
