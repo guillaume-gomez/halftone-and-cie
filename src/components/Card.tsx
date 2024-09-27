46f66bf0a952b94947b14a45e6207f4298addb4d
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-09-27 09:34:58 +0200

    update design card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,9 +8,11 @@ interface CardProps {
 
 function Card({ title, children, className = "bg-base-200" } : CardProps) {
     return (
-        <div className={`card ${className} border border-2`}>
+        <div className={`card ${className} border border-4`}>
           <div className="card-body">
-            <h2 className="card-title text-2xl">{title}</h2>
+            <div className="bg-neutral-300 p-2">
+                <h2 className="card-title text-2xl text-black">{title}</h2>
+            </div>
             {children}
           </div>
         </div>
