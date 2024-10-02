bf033d1bf01b9ac66081216b7441795a95cc6fb6
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-10-02 09:18:04 +0200

    change navbar design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ function Navbar() {
     return (
         <div className="navbar bg-accent text-primary-content py-5">
             <div className="border border-neutral border-2">
-                <img src="/logo.png" width="64" />
+                <img src="./logo.png" width="64" />
             </div>
             <button className="title btn btn-ghost text-3xl">Halftone and Cie</button>
         </div>
