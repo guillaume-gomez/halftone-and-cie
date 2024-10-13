880b7db1cd1c7019aaa0d942e14afa527517e1e9
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-10-13 13:49:11 +0200

    add tool function for one new filter

---
+++ src/point-layer.ts
@@ -0,0 +1,22 @@
+//usage
+// const lighterColor = newShade(`#000000`, 50); // Returns "#323232"
+// const lighterColor = newShade(`#ffffff`, -50); // Returns "#cdcdcd"
+
+function newShade(hexColor: string, magnitude: number) : string {
+    hexColor = hexColor.replace(`#`, ``);
+    if (hexColor.length === 6) {
+        const decimalColor = parseInt(hexColor, 16);
+        let r = (decimalColor >> 16) + magnitude;
+        r > 255 && (r = 255);
+        r < 0 && (r = 0);
+        let g = (decimalColor & 0x0000ff) + magnitude;
+        g > 255 && (g = 255);
+        g < 0 && (g = 0);
+        let b = ((decimalColor >> 8) & 0x00ff) + magnitude;
+        b > 255 && (b = 255);
+        b < 0 && (b = 0);
+        return `#${(g | (b << 8) | (r << 16)).toString(16)}`;
+    } else {
+        return hexColor;
+    }
+}
