4370478f243b8b5644cc34ba41f765299cdb1925
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-10-15 21:48:06 +0200

    fix the point layer

--- src/point-layer.ts
+++ src/point-layer.ts
@@ -1,6 +1,7 @@
+import { positionToDataIndex, getContext } from "./utils"
 //usage
 // const lighterColor = newShade(`#000000`, 50); // Returns "#323232"
-// const lighterColor = newShade(`#ffffff`, -50); // Returns "#cdcdcd"
+// const darkerColor = newShade(`#ffffff`, -50); // Returns "#cdcdcd"
 
 function newShade(hexColor: string, magnitude: number) : string {
     hexColor = hexColor.replace(`#`, ``);
@@ -15,26 +16,53 @@ function newShade(hexColor: string, magnitude: number) : string {
         let b = ((decimalColor >> 8) & 0x00ff) + magnitude;
         b > 255 && (b = 255);
         b < 0 && (b = 0);
-        return `#${(g | (b << 8) | (r << 16)).toString(16)}`;
+        return rgbToHex(r, g, b);
     } else {
         return hexColor;
     }
 }
 
-function createMaskPoints(canvasSource: HTMLCanvasElement, pointRadius: number) : void {
+function componentToHex(c: string) : string {
+  const hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r: number, g: number, b: number) : string {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+export function createMaskPoints(canvasSource: HTMLCanvasElement, pointRadius: number) : void {
+    console.log("myColor, ", newShade(`#ffffff`, -50))
+    console.log("myColor, ", newShade(`#000000`, -50))
     const { width, height } = canvasSource;
 
     const pointDiameterAndPadding = 2*(pointRadius) + 2*(pointRadius); // diameter + the padding
     const originX = width % pointDiameterAndPadding;
     const originY = height % pointDiameterAndPadding;
 
-    const context = canvasSource.getContext("2d");
+    const context = getContext(canvasSource);
 
-    for(let x=0; x < width; x += pointDiameterAndPadding) {
-        for(let y=0; y < height; y += pointDiameterAndPadding) {
+    const image = context.getImageData(
+        0,
+        0,
+        width,
+        height
+      );
+
+    for(let x = originX; x < width; x += pointDiameterAndPadding) {
+        for(let y = originY; y < height; y += pointDiameterAndPadding) {
+            const index = positionToDataIndex(x + pointRadius, y + pointRadius, width);
+            const [r, g, b] = [
+                image.data[index + 0],
+                image.data[index + 1],
+                image.data[index + 2],
+            ];
+
             context.beginPath();
-            context.arc(originX + x , originY + y ,pointRadius, 0, 2*Math.PI);
-            context.fillStyle = "red";
+            context.arc(x ,y ,pointRadius, 0, 2 * Math.PI);
+            const color = newShade(rgbToHex(r,g,b), -10);
+            //console.log("myColor, ", color)
+            context.fillStyle = color + "AA"; //"#00000010";
             context.fill();
         }
     }
