ffc56d94e972f1c9089fea54a6b18a1cdbe7e39b
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-10-21 09:25:27 +0200

    add lights

--- src/components/Metro/MetroHallway.tsx
+++ src/components/Metro/MetroHallway.tsx
@@ -1,7 +1,9 @@
 import MetroWall from "./MetroWall";
 import MetroCeil from "./MetroCeil";
 import MetroFloor from "./MetroFloor";
-import { Box } from '@react-three/drei';
+import { Box, useHelper } from '@react-three/drei';
+import { useRef } from "react";
+import { SpotLightHelper } from "three";
 
 interface MetroHallwayProps {
   position: [number, number, number];
@@ -11,8 +13,20 @@ interface MetroHallwayProps {
 }
 
 function MetroHallway({ position, width, height, depth }: MetroHallwayProps) {
+  const light = useRef()
+  useHelper(light, SpotLightHelper, 'cyan')
   return(
     <group position={position}>
+      {/*<Lightformer
+        form="rect" // circle | ring | rect (optional, default = rect)
+        intensity={1} // power level (optional = 1)
+        color="red" // (optional = white)
+        scale={[4, 1]} // Scale it any way you prefer (optional = [1, 1])
+        position={[0,height - 1,-10]}
+        target={[0, 0,-5]} // Target position (optional = undefined)
+      />*/}
+      <spotLight ref={light} position={[0,2, 2]}/>
+
       <MetroWall
         position={[0,0,0]}
         width={width}
