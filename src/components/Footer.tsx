a6c872a2c6ea6b2db971cad816e2a544f3607989
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-11-04 09:30:39 +0100

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,10 @@ function Footer() {
     return (
         <footer className="footer footer-center bg-base-300 text-base-content p-4">
           <aside>
-            <p>Made {new Date().getFullYear()} - By Guillaume Gomez</p>
+            <p>Made by {new Date().getFullYear()} - By Guillaume Gomez</p>
           </aside>
         </footer>
     );
 }
 export default Footer;
+
