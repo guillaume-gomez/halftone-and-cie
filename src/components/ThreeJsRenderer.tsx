8f19eaba4be87b1b8c2e700d58181a0cf08c9009
Guillaume Gomez <guillaume.gomez846@gmail.com>
2025-11-09 22:12:42 +0100

    prepare several scenes

--- src/components/ThreeJsRenderer.tsx
+++ src/components/ThreeJsRenderer.tsx
@@ -5,6 +5,7 @@ import { useFullscreen } from "rooks";
 import Ad from "./Ad";
 import Frame from "./Frame";
 import MetroHallway from "./Metro/MetroHallway";
+import SceneWithWalking from "./scenes/SceneWithWalking";
 
 interface ThreejsRenderingProps {
   texture?: string;
@@ -26,7 +27,6 @@ function ThreejsRendering({
   const { toggleFullscreen } = useFullscreen({ target: canvasRef });
   const cameraControllerRef = useRef<CameraControls|null>(null);
   const frameRef = useRef(null);
-  const backgroundColor = "purple";
   const originalCameraPosition = 20;
 
 
@@ -76,53 +76,12 @@ function ThreejsRendering({
         style={{width, height}}
         className={"hover:cursor-grab"}
       >
-        <color attach="background" args={[backgroundColor]} />
-        <ambientLight intensity={0.30} />
-        <CameraControls
-          makeDefault
-          smoothTime={0.25}
-          restThreshold={0.1}
-          ref={cameraControllerRef}
-        />
-        <Stage environment={null} adjustCamera={false} shadows="contact">
-          <Frame
-            position={[0,1, -25]}
-            widthTexture={widthTexture}
-            heightTexture={heightTexture}
-            ref={frameRef}
-            hasWindow={false}
-            hasBackLight={false}
-          >
-            <Ad
-              base64Texture={texture}
-              widthTexture={widthTexture}
-              heightTexture={heightTexture}
-            />
-          </Frame>
-          <Grid  args={[50, 50]} position={[0,-0.5,0]} cellColor='white' />
-        </Stage>
-
-        <MetroHallway
-          position={[0,0,2.4]}
-          width={6}
-          depth={30}
-          height={4}
-          hideFaces={["front"]}
-        />
-        <MetroHallway
-          position={[8,0,20.4]}
-          width={6}
-          depth={10}
-          height={4}
-          rotation={[0, Math.PI/2, 0]}
-          hideFaces={["back"]}
-        />
-        <MetroHallway
-          position={[0,0,20.4]}
-          width={6}
-          depth={6}
-          height={4}
-          hideFaces={["right", "back"]}
+        <SceneWithWalking
+          cameraRef={cameraControllerRef}
+          frameRef={frameRef}
+          widthTexture={widthTexture}
+          heightTexture={heightTexture}
+          texture={texture}
         />
 
         <Stats showPanel={0} className="stats"/>
---
+++ src/components/scenes/SceneWithWalking.tsx
@@ -0,0 +1,72 @@
+import { useRef , useEffect } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { CameraControls, Stage, Grid, Stats, GizmoHelper, GizmoViewport } from '@react-three/drei';
+import { useFullscreen } from "rooks";
+import Ad from "../Ad";
+import Frame from "../Frame";
+import MetroHallway from "../Metro/MetroHallway";
+
+interface SceneWithWalkingProps {
+  cameraRef: any
+  frameRef: any;
+  widthTexture: number;
+  heightTexture: number;
+  texture: string;
+}
+
+
+function SceneWithWalking({ cameraRef, frameRef, widthTexture, heightTexture, texture } : SceneWithWalkingProps) {
+	return (
+		<>
+		  <ambientLight intensity={0.30} />
+      <CameraControls
+        makeDefault
+        smoothTime={0.25}
+        restThreshold={0.1}
+        ref={cameraRef}
+      />
+      <Stage environment={null} adjustCamera={false} shadows="contact">
+        <Frame
+          position={[0,1, -25]}
+          widthTexture={widthTexture}
+          heightTexture={heightTexture}
+          ref={frameRef}
+          hasWindow={false}
+          hasBackLight={false}
+        >
+          <Ad
+            base64Texture={texture}
+            widthTexture={widthTexture}
+            heightTexture={heightTexture}
+          />
+        </Frame>
+        <Grid  args={[50, 50]} position={[0,-0.5,0]} cellColor='white' />
+      </Stage>
+
+      <MetroHallway
+        position={[0,0,2.4]}
+        width={6}
+        depth={30}
+        height={4}
+        hideFaces={["front"]}
+      />
+      <MetroHallway
+        position={[8,0,20.4]}
+        width={6}
+        depth={10}
+        height={4}
+        rotation={[0, Math.PI/2, 0]}
+        hideFaces={["back"]}
+      />
+      <MetroHallway
+        position={[0,0,20.4]}
+        width={6}
+        depth={6}
+        height={4}
+        hideFaces={["right", "back"]}
+      />
+    </>
+	);
+}
+
+export default SceneWithWalking;
